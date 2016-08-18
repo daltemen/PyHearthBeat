f362bf005e52758cf4b662b553d55682e3c25027
fava9410 <xxalexvargas@gmail.com>
2016-08-17 22:58:51 -0500

    correccion grafica

--- src/grafica.py
+++ src/grafica.py
@@ -7,17 +7,17 @@ client = pymongo.MongoClient()
 db = client.prueba
 
 #consultar todos los documentos
-#pulsos = db.pulsos.find()
+pulsos = db.pulsos.find()
 #Consultas filtradas para mejorar la presentacion de la grafica
-pulsos = db.pulsos.find({"procesamiento":{"$lt":0.00001}})
-
+#pulsos = db.pulsos.find({"procesamiento":{"$lt":0.00001}})
 tiemposProcesamiento = []
+
 #Se guarda solo el atributo con el que se va a trabajar
 for p in pulsos:
-	#tiemposProcesamiento.append(p["procesamiento"])
-	print(p["procesamiento"])
+	tiemposProcesamiento.append(p["procesamiento"])
+	
 #se define el eje x que son la cantidad de elementos
-#x = np.arange(1,len(tiemposProcesamiento)+1,1)
+x = np.arange(1,len(tiemposProcesamiento)+1,1)
 #grafica
-#plt.plot(x,tiemposProcesamiento, 'ro')
-#plt.show()
+plt.plot(x,tiemposProcesamiento, 'ro')
+plt.show()
