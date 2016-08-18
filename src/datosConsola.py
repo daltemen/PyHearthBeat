911f784b02a665f57cf83e0d0f8978aea65a34f4
fava9410 <xxalexvargas@gmail.com>
2016-08-17 22:55:08 -0500

    añadido script para guardar salida de consola

--- src/datosConsola.py
+++ src/datosConsola.py
@@ -11,13 +11,5 @@ db = client.prueba
 #Consultas filtradas para mejorar la presentacion de la grafica
 pulsos = db.pulsos.find({"procesamiento":{"$lt":0.00001}})
 
-tiemposProcesamiento = []
-#Se guarda solo el atributo con el que se va a trabajar
 for p in pulsos:
-	#tiemposProcesamiento.append(p["procesamiento"])
 	print(p["procesamiento"])
-#se define el eje x que son la cantidad de elementos
-#x = np.arange(1,len(tiemposProcesamiento)+1,1)
-#grafica
-#plt.plot(x,tiemposProcesamiento, 'ro')
-#plt.show()
